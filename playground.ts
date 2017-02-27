fb790193ee002c52be8e2309ccfe11f093b551ab
Can Abacigil <abacigil@gmail.com>
2017-02-27 18:48:06 +0300

    playground updated

--- playground.ts
+++ playground.ts
@@ -1,28 +1,28 @@
 import {TSParser} from './src/index'
 
 
-var query: string = `-- DROP PROCEDURE IF EXISTS \`country_hos\` DELIMITER !!;
-      DELIMITER      $$   -- comment
-CREATE PROCEDURE country_hos
-(IN con CHAR(20))
-BEGIN
-  SELECT Name, HeadOfState FROM Country
-  WHERE Continent = con;
-END
-$$
-DELIMITER ;
-SELECT * FROM users;
-SELECT * FROM user_details; -- COMMENT AREA
--- ;
--- DELIMITER $$
-DELIMITER //
-CREATE PROCEDURE country_hos_second
-(IN con CHAR(20))
-BEGIN
-  SELECT Name, HeadOfState FROM Country
-  WHERE Continent = con;
-END
-//`;
+// var query: string = `-- DROP PROCEDURE IF EXISTS \`country_hos\` DELIMITER !!;
+//       DELIMITER      $$   -- comment
+// CREATE PROCEDURE country_hos
+// (IN con CHAR(20))
+// BEGIN
+//   SELECT Name, HeadOfState FROM Country
+//   WHERE Continent = con;
+// END
+// $$
+// DELIMITER ;
+// SELECT * FROM users;
+// SELECT * FROM user_details; -- COMMENT AREA
+// -- ;
+// -- DELIMITER $$
+// DELIMITER //
+// CREATE PROCEDURE country_hos_second
+// (IN con CHAR(20))
+// BEGIN
+//   SELECT Name, HeadOfState FROM Country
+//   WHERE Continent = con;
+// END
+// //`;
 
 var query = `DELIMITER //
 CREATE PROCEDURE country_hos
@@ -39,5 +39,5 @@ DELIMITER ;`;
 var result: Array<string> = TSParser.parse(query, 'mysql', ';');
 
 result.forEach(element => {
-            console.log(element +'\n--ZZ--');
+            console.log(element +'\n--next--');
         });
